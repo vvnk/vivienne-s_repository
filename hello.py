0ed4b2fd090b994e7266ea006b40ab7bb2219063
Vivienne K <kamvivienne@gmail.com>
2026-04-06 17:31:59 +0000

    started project

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
-print("hello")
-print("yuki is a genius")
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('https://raw.githubusercontent.com/mk-gurucharan/Regression/master/IceCreamData.csv')
+
+X = dataset["Temperature"].values
+y = dataset["Revenue"].values
+
+dataset.head(5)
